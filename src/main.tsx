d2016e923fe82ef74918b223b40d779e2dbbd346
Jordan Garcia <jordidev@proton.me>
2025-12-24 12:20:34 -0600

    useState - Estado que re-dibuja

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from "react-dom/client";
 import { HooksApp } from "./HooksApp";
 
 import "./index.css";
+import { TrafficLight } from "./01-useState/TrafficLight";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HooksApp />
+    {/* <HooksApp /> */}
+    <TrafficLight />
   </StrictMode>
 );
