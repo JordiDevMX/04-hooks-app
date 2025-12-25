5ea0a08cef113eeabf650c5934540987b2badfc4
Jordan Garcia <jordidev@proton.me>
2025-12-25 16:39:16 -0600

    useRef - Valor que no dispara re-render

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { TrafficLight } from "./01-useState/TrafficLight";
 import { TrafficLightWithEffect } from "./02-useEffect/TrafficLightWithEffect";
 import { TrafficLightWithHook } from "./02-useEffect/TrafficLightWithHook";
 import { PokemonPage } from "./03-examples/PokemonPage";
+import { FocusScreen } from "./04-useRef/FocusScreen";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -14,6 +15,7 @@ createRoot(document.getElementById("root")!).render(
     {/* <TrafficLight /> */}
     {/* <TrafficLightWithEffect /> */}
     {/* <TrafficLightWithHook /> */}
-    <PokemonPage />
+    {/* <PokemonPage /> */}
+    <FocusScreen />
   </StrictMode>
 );
