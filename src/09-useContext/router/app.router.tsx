23b88159b5165cf503930e75eae1d41f3602c325
Jordan Garcia <jordidev@proton.me>
2026-01-06 21:35:07 -0600

    React Router - Manejo de rutas

---
+++ src/09-useContext/router/app.router.tsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter, Navigate } from "react-router";
+import { AboutPage } from "../pages/about/AboutPage";
+import { ProfilePage } from "../pages/profile/ProfilePage";
+import { LoginPage } from "../pages/Auth/LoginPage";
+
+export const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AboutPage />,
+  },
+  {
+    path: "/profile",
+    element: <ProfilePage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  // Si el usuario uso un link que no existe, redirigir a home (about page en este caso)
+  {
+    path: "*",
+    element: <Navigate to="/" />,
+  },
+]);
