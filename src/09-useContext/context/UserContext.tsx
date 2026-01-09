3354c69ec302ddec2febf197ce8b24c02a1f1f28
Jordan Garcia <jordidev@proton.me>
2026-01-08 23:47:48 -0600

    Contexto de usuario - Información global

--- src/09-useContext/context/UserContext.tsx
+++ src/09-useContext/context/UserContext.tsx
@@ -1,9 +1,50 @@
-import { useState, type PropsWithChildren } from "react";
+import { createContext, useState, type PropsWithChildren } from "react";
+import type { User } from "../data/user-mock.data";
+import { users } from "../data/user-mock.data";
 
-// * Hay mil maneras de tipar el children, pero esta es la mas eficiente de todas
+type AuthStatus = "checking" | "authenticated" | "not-authenticated";
+
+interface UserContextProps {
+  // State
+  authStatus: AuthStatus;
+  user: User | null;
+
+  // Methods
+  login: (userId: number) => boolean;
+  logout: () => void;
+}
+
+export const UserContext = createContext({});
+
+// Hay mil maneras de tipar el children, pero esta es la mas eficiente de todas ()
+// Los providers son HOC: Higher Order Component
 export const UserContextProvider = ({ children }: PropsWithChildren) => {
-  const [name, setName] = useState("Jordi");
-  // ? Se recomienda que el provider se use para logica de negocio,
-  // ? no para regresar una interfaz
-  return <>{children}</>;
+  const [authStatus, setAuthStatus] = useState("checking");
+  const [user, setUser] = useState<User | null>(null);
+
+  const handleLogin = (userId: number) => {
+    console.log({ userId });
+    return true;
+  };
+
+  const handleLogout = () => {
+    console.log("logout");
+  };
+
+  // Se recomienda que el provider se use para logica de negocio,
+  // no para regresar una interfaz
+  //   return <>{children}</>;
+
+  return (
+    <UserContext
+      value={{
+        authStatus: authStatus,
+        user: user,
+        login: handleLogin,
+        logout: handleLogout,
+      }}
+    >
+      {children}
+    </UserContext>
+  );
 };
